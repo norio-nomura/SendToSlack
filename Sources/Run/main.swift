294f83e9550b7a7355c1b8849aa4288e2fcc95b1
Norio Nomura <norio.nomura@gmail.com>
2018-05-21 11:28:12 +0900

    Add SIGTERM handler

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,11 @@
 import App
+import Foundation
+
+let signalHandler: @convention(c) (Int32) -> Swift.Void = { signo in
+    exit(128 + signo)
+}
+
+// https://devcenter.heroku.com/articles/dynos#shutdown
+signal(SIGTERM, signalHandler)
 
 try app(.detect()).run()
